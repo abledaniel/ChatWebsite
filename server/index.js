3b5d2b69dcf12ac72d982203125114a3dc007d7e
Abel Daniel <123517214+abledaniel@users.noreply.github.com>
2025-05-23 16:54:21 -0700

    Inital Commit with Websockets

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const ws = require('ws');
+const server = new ws.Server({port: 8080});
+
+server.on("connection", (socket) => {
+    socket.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+        socket.send(`Hello, you sent -> ${message}`);
+    })
+
+});
