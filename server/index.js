9d04fcba219be4c693b41b7299fd4c8667a4c70d
Abel Daniel <123517214+abledaniel@users.noreply.github.com>
2025-05-26 02:54:42 -0700

    Added Two way Communication using Websockets

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const ws = require('ws');
 const server = new ws.Server({port: 8080});
+const clients = new Set();
 
 server.on("connection", (socket) => {
+
+    console.log('New client connected');
+
+    clients.add(socket);
     socket.on('message', (message) => {
         console.log(`Received message: ${message}`);
-        socket.send(`Hello, you sent -> ${message}`);
-    })
+        socket.send(`You said: ${message}`);
+        
+        for (client of clients) {
+            if (client !== socket && client.readyState === WebSocket.OPEN){
+                client.send(`Other said: ${message}`);
+            }
+        }
+    });
+
+    socket.on("close",() => {
+        console.log('Client disconnected');
+        clients.delete(socket);
+    });
+
+
 
 });
